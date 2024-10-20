a0fa41072cf6966484d45463d43e01933d78d5e4
Starinin Andrey <starininandrey@gmail.com>
2024-10-20 11:00:31 +0300

    Initial commit

---
+++ DispatcherProcesses/MainWindowModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace DispatcherProcesses;
+
+public class MainWindowModel : BaseWindowModel
+{
+    public ObservableCollection<Process> Processes { get; private set; } = [];
+
+    public LambdaCommand RefreshCommand { get; }
+    
+    
+    public MainWindowModel()
+    {
+        RefreshCommand = new LambdaCommand(execute: (_) =>
+        {
+            RefreshProcesses(Processes, Process.GetProcesses());
+        });
+    }
+
+    private void RefreshProcesses(ObservableCollection<Process> processes, Process[] processesArray)
+    {
+        processes.Clear();
+        foreach (var process in processesArray)
+        {
+            processes.Add(process);
+        }
+    }
+}
---
+++ DispatcherProcesses/ProcessArrayExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace DispatcherProcesses;
+
+public static class ProcessArrayExtension
+{
+    public static ObservableCollection<Process> ToObservableCollection(this Process[] processes)
+    {
+        var observableCollection = new ObservableCollection<Process>();
+        foreach (var process in processes)
+        {
+            observableCollection.Add(process);
+        }
+        return observableCollection;
+    }
+}
